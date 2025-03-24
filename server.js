a6cf7548b26122a3a71db82b9bae4b20e486102c
Lisboa <joaovitorlisboasilva@gmail.com>
2025-03-24 20:24:07 -0300

    fix: Set host option on listen

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ server.delete('/videos/:id', async (request, reply) => {
 })
 
 server.listen({
+    host: '0.0.0.0',
     port: process.env.PORT ?? 3333,
 }).then(() => {
     console.log(`HTTP server running! Port: ${process.env.PORT ?? 3333}`)
